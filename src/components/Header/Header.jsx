27b59c87b44fc1695c8f438d5f8803aa24b29f52
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2022-01-30 20:40:51 +0100

    Header created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import Logo from './components/Logo/Logo';
+import Button from '../../common/Button/Button';
+import './Header.css';
+import { useState } from 'react';
+
+export default function Header() {
+	const [userName, setUserName] = useState('defaultUserName');
+	return (
+		<header>
+			<div className='left'>
+				<Logo />
+				<span>Courses</span>
+			</div>
+			<div className='right'>
+				<span className='right-item'>{userName}</span>
+				<Button
+					className='right-item'
+					buttonText='Logout'
+					onClick={(e) => console.log(e.target.innerHTML, 'button clicked!')}
+				/>
+			</div>
+		</header>
+	);
+}
