1dd9142ed8a34a969bfd38b97409e4d212b5f193
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2022-01-30 20:38:32 +0100

    Initial changes and App component modified

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header/Header';
+import './App.css';
+import Courses from './components/Courses/Courses';
+
+function App() {
+	return (
+		<div className='App'>
+			React
+			<Header />
+			<Courses />
+		</div>
+	);
+}
+
+export default App;
