223d737ae8e50f222c87bc5747c95d1e333374f3
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2022-01-30 20:41:08 +0100

    Logo component created

---
+++ src/components/Header/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import './Logo.css';
+
+export default function Logo() {
+	return (
+		<img
+			src='https://th.bing.com/th/id/R.3db6715a0fa0954982b72970bd4acbeb?rik=ylvwr7WY55l6XQ&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_179623.png&ehk=lKqU5%2bWWwy1cZZt7RMRAl0O%2bChKMHjaf7nXz71U3rmo%3d&risl=&pid=ImgRaw&r=0'
+			alt='courses'
+		/>
+	);
+}
