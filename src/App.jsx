181d3a41119c96643b15ab301b45ff9b8e30288e
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2022-02-06 22:38:11 +0100

    Registration component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Header from './components/Header/Header';
 import Courses from './components/Courses/Courses';
 
 import './App.css';
+import Registration from './components/Registration/Registration';
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
-			<Courses />
+			<Registration />
 		</div>
 	);
 }
---
+++ src/components/Registration/Registration.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+
+import { Button } from '../../common/Button/Button';
+import Input from '../../common/Input/Input';
+
+import './Registration.css';
+
+export default function Registration(props) {
+	const [userName, setUserName] = useState('');
+	const [userEmail, setUserEmail] = useState('');
+	const [userPassword, setUserPassword] = useState('');
+
+	function handleRegister() {
+		if (userName.length < 2) {
+			alert('User name should be at least 2 characters long');
+			return;
+		}
+		// TO DO: EMAIL VALIDATION
+		if (userPassword.length < 2) {
+			alert('User password should be at least 2 characters long');
+			return;
+		}
+	}
+
+	return (
+		<div className='container'>
+			<form className='form'>
+				<h2>Registration</h2>
+				<Input
+					id='name'
+					labelText='Name'
+					labelClassName='label'
+					placeholder='Enter name'
+					value={userName}
+					onInput={(e) => setUserName(e.target.value)}
+				/>
+				<Input
+					labelText='Email'
+					labelClassName='label'
+					placeholder='Enter email'
+					value={userEmail}
+					onInput={(e) => setUserEmail(e.target.value)}
+				/>
+				<Input
+					labelText='Password'
+					labelClassName='label'
+					placeholder='Enter password'
+					value={userPassword}
+					onInput={(e) => setUserPassword(e.target.value)}
+				/>
+				<Button
+					className='button'
+					buttonText='Registration'
+					onClick={handleRegister}
+				/>
+				<div className='bottom-text'>
+					If you have an account you can <a href=''>Login</a>
+				</div>
+			</form>
+		</div>
+	);
+}
