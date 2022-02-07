194c0984eda96077f65612ae5d19ac28d270aeeb
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2022-02-07 23:12:04 +0100

    AppContext implemented for authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import Header from './components/Header/Header';
 import Courses from './components/Courses/Courses';
 
 import './App.css';
-import Registration from './components/Registration/Registration';
+import { AppContext } from './AppContext';
 
 function App() {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
 	return (
 		<div className='App'>
+				<AppContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
 			<Header />
 			<Registration />
 		</div>
+				</AppContext.Provider>
 	);
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,20 +8,24 @@ import './Header.css';
 export default function Header() {
 	// eslint-disable-next-line no-unused-vars
 	const [userName, setUserName] = useState('someUserName');
+	const { isAuthenticated, setIsAuthenticated } = useAppContext();
+	
 	return (
 		<header>
 			<div className='left'>
 				<Logo />
 				<span>Courses</span>
 			</div>
-			<div className='right'>
-				<span className='right-item'>{userName}</span>
-				<Button
-					className='right-item'
-					buttonText='Logout'
-					onClick={(e) => console.log(e.target.innerHTML, 'button clicked!')}
-				/>
-			</div>
+			{!isAuthenticated && (
+				<div className='right'>
+					<span className='right-item'>{userName}</span>
+					<Button
+						className='right-item'
+						buttonText='Logout'
+						onClick={(e) => console.log(e.target.innerHTML, 'button clicked!')}
+					/>
+				</div>
+			)}
 		</header>
 	);
 }
