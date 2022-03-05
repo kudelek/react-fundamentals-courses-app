23f6531f69d0b6be1f68ea45647cb50d7f1fb711
Konrad Zapotoczny <Konrad_Zapotoczny@epam.com>
2022-03-05 02:39:55 +0100

    CourseForm tests created, most of them pass, but one doesn't - TO DO

---
+++ src/components/CourseForm/tests/courseForm.test.js
@@ -0,0 +1,102 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom/cjs/react-router-dom.min';
+import { mockedAuthorsList, mockedCoursesList } from '../../../constants';
+import CourseForm from '../CourseForm';
+
+const mockedState_C_A = {
+	user: {
+		isAuth: true,
+		name: 'Test Name',
+		token: 'test',
+		role: 'admin',
+	},
+	courses: [mockedCoursesList[0]],
+	authors: mockedAuthorsList,
+};
+
+const mockedStore_C_A = {
+	getState: () => mockedState_C_A,
+	subscribe: jest.fn(),
+	dispatch: jest.fn(),
+};
+
+// const spy = jest.spyOn(redux, 'useSelector');
+// spy.mockReturnValue(mockedState_C_A.getState().courses);
+
+describe('CourseForm component tests', () => {
+	it('should show authors lists (all and course authors)', () => {
+		render(
+			<Provider store={mockedStore_C_A}>
+				<Router exact path='/courses/add'>
+					<CourseForm />
+				</Router>
+			</Provider>
+		);
+		const input = screen.queryByTestId('author-to-be-created');
+
+		expect(screen.queryAllByTestId('author')).toHaveLength(
+			mockedAuthorsList.length
+		);
+		expect(screen.queryAllByTestId('course-author')).toHaveLength(0);
+		expect(screen.queryByTestId('author-to-be-created').innerHTML).toMatch('');
+
+		fireEvent.change(input, {
+			target: { value: 'John Smith' },
+		});
+		expect(screen.queryByTestId('author-to-be-created').innerHTML).toMatch(
+			'John Smith'
+		);
+
+		fireEvent.click(screen.getByTestId('create-author'));
+		expect(mockedStore_C_A.dispatch).toBeCalled();
+	});
+
+	it(`'Add author' button click should add an author to course author list`, () => {
+		render(
+			<Provider store={mockedStore_C_A}>
+				<Router exact path='/courses/add'>
+					<CourseForm />
+				</Router>
+			</Provider>
+		);
+
+		expect(screen.queryAllByTestId('author')).toHaveLength(
+			mockedAuthorsList.length
+		);
+		expect(screen.queryAllByTestId('course-author')).toHaveLength(0);
+
+		fireEvent.click(screen.queryAllByTestId('add-course-author')[0]);
+
+		expect(screen.queryAllByTestId('author')).toHaveLength(
+			mockedAuthorsList.length - 1
+		);
+		expect(screen.queryAllByTestId('course-author')).toHaveLength(1);
+	});
+
+	it(`'Delete author' button click should remove an author from course author list`, () => {
+		render(
+			<Provider store={mockedStore_C_A}>
+				<Router exact path='/courses/add'>
+					<CourseForm />
+				</Router>
+			</Provider>
+		);
+
+		expect(screen.queryAllByTestId('author')).toHaveLength(
+			mockedAuthorsList.length
+		);
+		expect(screen.queryAllByTestId('course-author')).toHaveLength(0);
+
+		fireEvent.click(screen.queryAllByTestId('add-course-author')[0]);
+		fireEvent.click(screen.queryAllByTestId('add-course-author')[0]);
+		expect(screen.queryAllByTestId('course-author')).toHaveLength(2);
+
+		fireEvent.click(screen.queryAllByTestId('remove-course-author')[0]);
+
+		expect(screen.queryAllByTestId('author')).toHaveLength(
+			mockedAuthorsList.length - 1
+		);
+		expect(screen.queryAllByTestId('course-author')).toHaveLength(1);
+	});
+});
